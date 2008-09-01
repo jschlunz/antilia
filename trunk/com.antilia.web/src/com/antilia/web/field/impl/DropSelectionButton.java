1c3099ef1fdc992c117e433278ff6d1a72f0226e
reiern70 <reiern70@0c16656c-7d50-0410-8e61-d121633fcfaf>
2008-09-01 14:00:15 +0000

    export to PDF improved

--- trunk/com.antilia.web/src/com/antilia/web/field/impl/DropSelectionButton.java
+++ trunk/com.antilia.web/src/com/antilia/web/field/impl/DropSelectionButton.java
@@ -4,9 +4,18 @@
  */
 package com.antilia.web.field.impl;
 
+import java.io.Serializable;
+
+import org.apache.wicket.Component;
 import org.apache.wicket.ResourceReference;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.util.lang.PropertyResolver;
 
 import com.antilia.web.button.AbstractButton;
+import com.antilia.web.field.BeanProxy;
+import com.antilia.web.field.IFieldModel;
+import com.antilia.web.field.IFieldPanel;
 import com.antilia.web.resources.DefaultStyle;
 
 /**
@@ -15,24 +24,37 @@ import com.antilia.web.resources.DefaultStyle;
  * @author Ernesto Reinaldo Barreiro (reiern70@gmail.com)
  *
  */
-public class DropSelectionButton extends AbstractButton {
+public class DropSelectionButton<B extends Serializable> extends AbstractButton {
 
 	
 	private static final long serialVersionUID = 1L;
 
+	private BeanProxy<B> beanProxy; 
+	private IFieldModel<B> fieldModel; 
+	private IFieldPanel fieldPanel;
+	
 	/**
 	 * @param id
 	 * @param ajaxButton
 	 */
-	public DropSelectionButton(String id) {
-		super(id, true);
+	public DropSelectionButton(String id, BeanProxy<B> beanProxy, IFieldModel<B> fieldModel) {
+		super(id, true);		
+		this.beanProxy = beanProxy;
+		this.fieldModel = fieldModel;
 	}
 
 	
 	@Override
-	public void onSubmit() {
-		
+	protected void onSubmit(AjaxRequestTarget target, Form form) {
+		if(this.fieldPanel == null)
+			this.fieldPanel = findFieldPanel();
+		String propertyName = fieldModel.getPropertyPath();
+		PropertyResolver.setValue(propertyName, beanProxy.getBean(), null, null);
+		if(target != null) {
+			target.addComponent((Component) fieldPanel);
+		}
 	}
+	
 	
 	/* (non-Javadoc)
 	 * @see com.antilia.web.button.AbstractButton#getImage()
@@ -51,5 +73,8 @@ public class DropSelectionButton extends AbstractButton {
 	}
 	
 	
+	private IFieldPanel findFieldPanel() {
+		return (IFieldPanel)findParent(IFieldPanel.class);
+	}
 
 }
