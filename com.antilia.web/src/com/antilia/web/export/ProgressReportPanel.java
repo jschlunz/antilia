219eaf4eeda4f54d63c1c62b75bcd4d59bf2910c
reiern70 <reiern70@0c16656c-7d50-0410-8e61-d121633fcfaf>
2008-11-01 06:21:35 +0000

    http://code.google.com/p/antilia/issues/detail?id=5

--- com.antilia.web/src/com/antilia/web/export/ProgressReportPanel.java
+++ com.antilia.web/src/com/antilia/web/export/ProgressReportPanel.java
@@ -3,11 +3,17 @@
  */
 package com.antilia.web.export;
 
+import org.apache.wicket.ResourceReference;
+import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.panel.Panel;
 
 import com.antilia.hibernate.context.IProgressReporter;
+import com.antilia.web.dialog.CloseDialogButton;
+import com.antilia.web.dialog.DefaultDialog;
 import com.antilia.web.progress.HtmlProgressBar;
+import com.antilia.web.resources.DefaultStyle;
 
 /**
  * @author EReinaldoB
@@ -19,10 +25,13 @@ public class ProgressReportPanel extends Panel {
 
 	private IProgressReporter progresReporter;
 	
+	private CloseDialogButton cancelButton;
 	
 	public ProgressReportPanel(String id, IProgressReporter progresReporter) {
 		super(id);
 		this.progresReporter = progresReporter;
+		
+		
 	}
 	
 	@Override
@@ -41,5 +50,33 @@ public class ProgressReportPanel extends Panel {
 			}
 		};	
 		addOrReplace(progress);
+		
+		if(cancelButton == null){
+			cancelButton = new CloseDialogButton("cancelButton", findDefaultDialog()) {
+
+				private static final long serialVersionUID = 1L;
+				
+				@Override
+				protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+					super.onSubmit(target, form);
+					progresReporter.cancelJob();
+				}
+				
+				@Override
+				protected ResourceReference getImage() {
+					return DefaultStyle.IMG_CANCEL;
+				}
+				
+				@Override
+				protected String getLabel() {
+					return "Cancel";
+				}
+			};
+			add(cancelButton);
+		}
+	}
+	
+	private DefaultDialog findDefaultDialog() {
+		return findParent(DefaultDialog.class);
 	}
 }
