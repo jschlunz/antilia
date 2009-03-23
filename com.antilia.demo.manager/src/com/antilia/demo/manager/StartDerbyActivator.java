3406297e32b64233c6ff1f288d2e7326879e8669
reiern70 <reiern70@0c16656c-7d50-0410-8e61-d121633fcfaf>
2009-03-23 14:18:01 +0000

    added ddlutils to build database

---
+++ com.antilia.demo.manager/src/com/antilia/demo/manager/StartDerbyActivator.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package com.antilia.demo.manager;
+
+import java.io.PrintWriter;
+
+import org.apache.derby.drda.NetworkServerControl;
+import org.osgi.framework.BundleContext;
+
+import com.antilia.common.osgi.IServiceActivator;
+
+/**
+ * @author Juan Carlos López Artigas
+ *
+ */
+public class StartDerbyActivator implements IServiceActivator {
+
+	NetworkServerControl derby = null;
+	
+	public StartDerbyActivator() {
+		try {
+			derby = new NetworkServerControl("reiern","reiern");
+		} catch (Exception e) {			
+			e.printStackTrace();
+		}
+	}
+	/* (non-Javadoc)
+	 * @see com.antilia.common.osgi.IServiceActivator#isMandatory()
+	 */
+	public boolean isMandatory() throws Exception {
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.antilia.common.osgi.IServiceActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		derby.start(new PrintWriter(System.out));
+	}
+
+	/* (non-Javadoc)
+	 * @see com.antilia.common.osgi.IServiceActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		derby.shutdown();
+	}
+
+}
