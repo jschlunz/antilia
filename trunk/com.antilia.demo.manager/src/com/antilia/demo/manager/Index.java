a34be67e95cb28959be3005f2dc1677f9f5d34e8
reiern70 <reiern70@0c16656c-7d50-0410-8e61-d121633fcfaf>
2009-03-31 19:18:11 +0000

    re-factoring CRUD base page to be moe general.

--- trunk/com.antilia.demo.manager/src/com/antilia/demo/manager/Index.java
+++ trunk/com.antilia.demo.manager/src/com/antilia/demo/manager/Index.java
@@ -4,10 +4,9 @@
  */
 package com.antilia.demo.manager;
 
-import org.apache.wicket.markup.html.WebMarkupContainer;
-
-import com.antilia.web.layout.FullPage;
+import com.antilia.web.layout.ToolBarFullPage;
 import com.antilia.web.login.IProtectedPage;
+import com.antilia.web.toolbar.Toolbar;
 
 
 /**
@@ -15,53 +14,18 @@ import com.antilia.web.login.IProtectedPage;
  * @author Ernesto Reinaldo Barreiro (reiern70@gmail.com)
  *
  */
-public class Index extends FullPage implements IContainer, IProtectedPage {
+public class Index extends ToolBarFullPage implements IProtectedPage {
 
 	private static final long serialVersionUID = 1L;	
-	
-	private WebMarkupContainer content;
-	
-	private WebMarkupContainer body;
 	
 	public Index() {		
 		super();			
 		
-		body = new WebMarkupContainer("body");
-		body.setOutputMarkupId(true);
-		
-		add(body);
-		
-		content = new InitialPanel("content", this);				
 	}
 
 	@Override
-	protected void onBeforeRender() {
-		super.onBeforeRender();
-		
-		body.addOrReplace(content);
-	
+	protected Toolbar createToolbar(String id, ToolBarFullPage page,
+			String contentId) {
+		return Toolbar.createToolbar(id, new MainMenuFactory(this, contentId));
 	}
-	
-	/**
-	 * @return the content
-	 */
-	public WebMarkupContainer getContent() {
-		return content;
-	}
-
-	/**
-	 * @param content the content to set
-	 */
-	public void setContent(WebMarkupContainer content) {
-		this.content = content;
-	}
-
-
-	/**
-	 * @return the body
-	 */
-	public WebMarkupContainer getBody() {
-		return body;
-	}
-	
 }
