9c779dd9b103e1e566c6f30e2fca1210a078aa41
reiern70 <reiern70@0c16656c-7d50-0410-8e61-d121633fcfaf>
2009-05-06 10:10:20 +0000

    added some utility methods that allow to filer a list according to a filter

---
+++ com.antilia.hibernate/src/com/antilia/hibernate/util/QueryUtils.java
@@ -0,0 +1,56 @@
+package com.antilia.hibernate.util;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.antilia.hibernate.query.IFilter;
+import com.antilia.hibernate.query.IQuery;
+import com.antilia.hibernate.query.IlikeRestriction;
+
+/**
+ * @author Ernesto Reinaldo Barreiro (reirn70@gmail.com)
+ *
+ */
+public class QueryUtils {
+
+	public static <T extends Serializable> T matchQuery(T bean, IQuery<T> query) {
+		if(bean == null )
+			return null;
+		if(query == null)
+			return bean;
+		for(IFilter filter: query.getFilters()) {
+			if(filter instanceof IlikeRestriction) {
+				IlikeRestriction restriction = (IlikeRestriction)filter;
+				String propertyName = restriction.getPropertyName();				
+				try {
+					Object rValue  = restriction.getValue();
+					if(rValue != null) {
+						Object value = EntityUtils.getPropertyValue(bean, propertyName);
+						String strVale = value.toString();
+						if(!strVale.contains(rValue.toString())) {
+							return null;
+						}
+					}
+				} catch (Exception e) {					
+					return null;
+				}
+			}
+		}
+		return bean;
+	}
+	
+	
+	public static <T extends Serializable> List<T> findSubList(List<T> list, IQuery<T> query) {
+		if(list == null)
+			return null;
+		List<T> nlist = new ArrayList<T>();
+		for(T bean: list) {
+			T result = matchQuery(bean, query);
+			if(result != null) {
+				nlist.add(bean);
+			}
+		}
+		return nlist;
+	}
+}
