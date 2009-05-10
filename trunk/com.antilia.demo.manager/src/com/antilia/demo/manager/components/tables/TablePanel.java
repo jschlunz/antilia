a63f5f135c6cd77ae47d64a1d21b3da053c47cc1
reiern70 <reiern70@0c16656c-7d50-0410-8e61-d121633fcfaf>
2009-05-10 07:59:15 +0000

    page for different selection models

--- trunk/com.antilia.demo.manager/src/com/antilia/demo/manager/components/tables/TablePanel.java
+++ trunk/com.antilia.demo.manager/src/com/antilia/demo/manager/components/tables/TablePanel.java
@@ -9,6 +9,7 @@ import org.apache.wicket.markup.html.panel.Panel;
 import com.antilia.demo.manager.components.beans.Person;
 import com.antilia.web.beantable.Table;
 import com.antilia.web.beantable.model.TableModel;
+import com.antilia.web.beantable.provider.SelectionMode;
 
 /**
  * @author Ernesto Reinaldo Barreiro (reiern70@gmail.com)
@@ -28,6 +29,14 @@ public class TablePanel extends Panel {
 		super(id);
 		TableModel<Person> tableModel = new TableModel<Person>(Person.class, "id", "name", "lastName1");
 		add(new Table<Person>("table",tableModel, Person.createPersons()));
+		
+		TableModel<Person> tableModel1 = new TableModel<Person>(Person.class, "id", "name", "lastName1");
+		tableModel1.setSelectionModel(SelectionMode.SINGLE);
+		add(new Table<Person>("table1",tableModel1, Person.createPersons()));
+		
+		TableModel<Person> tableModel2 = new TableModel<Person>(Person.class, "id", "name", "lastName1");
+		tableModel2.setSelectionModel(SelectionMode.NONE);
+		add(new Table<Person>("table2",tableModel2, Person.createPersons()));
 	}
 	
 }
