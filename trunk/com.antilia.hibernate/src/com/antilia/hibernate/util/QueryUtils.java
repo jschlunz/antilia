7750ce06d4cedb1988228a0f2d5cd905276eecaf
reiern70 <reiern70@0c16656c-7d50-0410-8e61-d121633fcfaf>
2009-05-26 12:50:22 +0000

    sorting added to list

--- trunk/com.antilia.hibernate/src/com/antilia/hibernate/util/QueryUtils.java
+++ trunk/com.antilia.hibernate/src/com/antilia/hibernate/util/QueryUtils.java
@@ -2,11 +2,16 @@ package com.antilia.hibernate.util;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
+import com.antilia.common.util.ReflectionUtils;
 import com.antilia.hibernate.query.IFilter;
+import com.antilia.hibernate.query.IOrder;
 import com.antilia.hibernate.query.IQuery;
 import com.antilia.hibernate.query.IlikeRestriction;
+import com.antilia.hibernate.query.IOrder.OrderType;
 
 /**
  * @author Ernesto Reinaldo Barreiro (reirn70@gmail.com)
@@ -14,6 +19,32 @@ import com.antilia.hibernate.query.IlikeRestriction;
  */
 public class QueryUtils {
 
+	private static class IOrderComparator<T extends Serializable> implements Comparator<T> {
+		
+		private IOrder<T> order;
+		
+		public IOrderComparator(IOrder<T> order) {
+			this.order = order;
+		}
+		
+		@SuppressWarnings("unchecked")
+		public int compare(T o1, T o2) {
+			try {
+				Object ov1 = ReflectionUtils.getPropertyValue(o1, order.getPropertyPath());
+				Object ov2 = ReflectionUtils.getPropertyValue(o2, order.getPropertyPath());
+				if(ov1 instanceof Comparable && ov2 instanceof Comparable){
+					Comparable c1 = (Comparable)ov1;
+					Comparable c2 = (Comparable)ov2;
+					if(order.getType().equals(OrderType.ASCENDING))
+						return c1.compareTo(c2);
+					else if(order.getType().equals(OrderType.DESCENDING))
+						return c2.compareTo(c1);
+				}				
+			}catch (Exception e) {				
+			}
+			return 0;
+		};
+	}
 	public static <T extends Serializable> T matchQuery(T bean, IQuery<T> query) {
 		if(bean == null )
 			return null;
@@ -39,7 +70,18 @@ public class QueryUtils {
 		}
 		return bean;
 	}
+		
 	
+	public static <T extends Serializable> List<T> sortList(List<T> list, IQuery<T> query) {
+		IOrderComparator<T> comparator = null;
+		for(IOrder<T> order : query.getOrders()) {
+			comparator = new IOrderComparator<T>(order);
+		}
+		if(comparator != null) {
+			Collections.sort(list, comparator);
+		}
+		return list;
+	}
 	
 	public static <T extends Serializable> List<T> findSubList(List<T> list, IQuery<T> query) {
 		if(list == null)
@@ -51,6 +93,6 @@ public class QueryUtils {
 				nlist.add(bean);
 			}
 		}
-		return nlist;
+		return sortList(nlist, query);
 	}
 }
