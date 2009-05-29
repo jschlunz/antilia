81b5850f7b4e4e721eb7137e4502a6efcc7555c9
reiern70 <reiern70@0c16656c-7d50-0410-8e61-d121633fcfaf>
2009-05-29 11:50:01 +0000

    now tables that receive a collection as constructor argument will be sortable by default

--- trunk/com.antilia.demo.manager/src/com/antilia/demo/manager/components/tables/TablePanel.java
+++ trunk/com.antilia.demo.manager/src/com/antilia/demo/manager/components/tables/TablePanel.java
@@ -4,13 +4,15 @@
  */
 package com.antilia.demo.manager.components.tables;
 
+import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.Model;
 
 import com.antilia.demo.manager.components.beans.Person;
 import com.antilia.web.beantable.Table;
 import com.antilia.web.beantable.model.IColumnModel;
 import com.antilia.web.beantable.model.TableModel;
-import com.antilia.web.provider.SelectionMode;
+import com.antilia.web.field.impl.AjaxCheckBox;
 
 /**
  * @author Ernesto Reinaldo Barreiro (reiern70@gmail.com)
@@ -23,12 +25,15 @@ public class TablePanel extends Panel {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	private Table<Person> table;	
+	private TableModel<Person> tableModel;
+	
 	/**
 	 * @param id
 	 */
 	public TablePanel(String id) {
 		super(id);
-		TableModel<Person> tableModel = new TableModel<Person>(Person.class, "id", "name", "lastName1") {
+		tableModel = new TableModel<Person>(Person.class, "id", "name", "lastName1") {
 			
 			private static final long serialVersionUID = 1L;
 
@@ -36,27 +41,105 @@ public class TablePanel extends Panel {
 			protected IColumnModel<Person> createColumnModel(String expresion) {
 				IColumnModel<Person> columnModel =  super.createColumnModel(expresion);
 				if(expresion.equals("id")) {
-					columnModel.setWidth(80);
-					columnModel.setSortable(false);
-					columnModel.setResizable(false);
-				} else {
-					columnModel.setSortable(false);
-					columnModel.setResizable(false);
-				}
+					columnModel.setWidth(80);				
+				} 
 				return columnModel;
 			}
 		};
-		Table<Person> table = new Table<Person>("table",tableModel, Person.createPersons());
-		table.setFirstColumnResizable(false);
-		add(table);
 		
-		TableModel<Person> tableModel1 = new TableModel<Person>(Person.class, "id", "name", "lastName1");
-		tableModel1.setSelectionMode(SelectionMode.SINGLE);
-		add(new Table<Person>("table1",tableModel1, Person.createPersons()));
+		AjaxCheckBox draggableColoumns = new AjaxCheckBox("draggableColoumns", 
+				new Model<Boolean>() {
+
+					private static final long serialVersionUID = 1L;
+					
+					@Override
+					public Boolean getObject() {
+						return table.isDragableColumns();
+					}
+					
+					@Override
+					public void setObject(Boolean object) {
+						table.setDragableColumns(object);
+					}
+			
+					}) {
+	
+			private static final long serialVersionUID = 1L;
 		
-		TableModel<Person> tableModel2 = new TableModel<Person>(Person.class, "id", "name", "lastName1");
-		tableModel2.setSelectionMode(SelectionMode.NONE);
-		add(new Table<Person>("table2",tableModel2, Person.createPersons()));
+			@Override
+			protected void onUpdate(AjaxRequestTarget target) {
+				if(target != null) {
+					target.addComponent(table.getUpdatableComponent());
+				}
+			}
+		}; 
+		
+		add(draggableColoumns);
+		
+		AjaxCheckBox resizableColumns = new AjaxCheckBox("resizableColumns", 
+				new Model<Boolean>() {
+
+					private static final long serialVersionUID = 1L;
+					
+					@Override
+					public Boolean getObject() {
+						return table.isColumnsResizable();
+					}
+					
+					@Override
+					public void setObject(Boolean object) {
+						table.setColumnsResizable(object);
+					}
+			
+					}) {
+	
+			private static final long serialVersionUID = 1L;
+		
+			@Override
+			protected void onUpdate(AjaxRequestTarget target) {
+				if(target != null) {
+					target.addComponent(table.getUpdatableComponent());
+				}
+			}
+		}; 
+		
+		add(resizableColumns);
+		
+		AjaxCheckBox fresizableColumns = new AjaxCheckBox("fresizableColumns", 
+				new Model<Boolean>() {
+
+					private static final long serialVersionUID = 1L;
+					
+					@Override
+					public Boolean getObject() {
+						return table.isFirstColumnResizable();
+					}
+					
+					@Override
+					public void setObject(Boolean object) {
+						table.setFirstColumnResizable(object);
+					}
+			
+					}) {
+	
+			private static final long serialVersionUID = 1L;
+		
+			@Override
+			protected void onUpdate(AjaxRequestTarget target) {
+				if(target != null) {
+					target.addComponent(table.getUpdatableComponent());
+				}
+			}
+		}; 
+		
+		add(fresizableColumns);
+		
+		
+		table = new Table<Person>("table",tableModel, Person.createPersons());
+		table.setFirstColumnResizable(true);
+		table.setColumnsResizable(true);
+		table.setDragableColumns(true);		
+		add(table);		
 	}
 	
 }
