fca713c26f53323bb0f44280ffae381cd3fef450
reiern70 <reiern70@0c16656c-7d50-0410-8e61-d121633fcfaf>
2009-06-19 13:12:55 +0000

    support for iBatis and Spring

---
+++ com.antilia.demo.manager/src/com/antilia/demo/manager/ibatis/IBatisDaoLocator.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.antilia.demo.manager.ibatis;
+
+import org.apache.wicket.injection.web.InjectorHolder;
+
+import com.antilia.hibernate.dao.impl.IBatisAggregatedDaoLocator;
+
+/**
+ * @author Ernesto Reinaldo Barreiro (reirn70@gmail.com)
+ *
+ */
+public class IBatisDaoLocator extends IBatisAggregatedDaoLocator {
+	
+	private static IBatisDaoLocator instance;
+
+	
+	/**
+	 * 
+	 */
+	private IBatisDaoLocator() {				
+	}
+	
+	@Override
+	protected void initialize() {
+		InjectorHolder.getInjector().inject(this);
+	}
+
+	/**
+	 * @return the instance
+	 */
+	public static IBatisDaoLocator getInstance() {
+		if(instance== null) {
+			instance = new IBatisDaoLocator();
+		}
+		return instance;
+	}
+}
