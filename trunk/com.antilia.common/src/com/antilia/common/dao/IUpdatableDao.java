a60cc8f31dfce1b97aacf457e8064c0f6acb595c
reiern70 <reiern70@0c16656c-7d50-0410-8e61-d121633fcfaf>
2009-06-23 15:16:50 +0000

    clean Query related functionality off Hibernate references

---
+++ trunk/com.antilia.common/src/com/antilia/common/dao/IUpdatableDao.java
@@ -0,0 +1,30 @@
+package com.antilia.common.dao;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+/**
+ * 
+ * @author Ernesto Reinaldo Barreiro (reirn70@gmail.com)
+ *
+ */
+public interface IUpdatableDao<E extends Serializable> extends Serializable {
+	 
+	public E add(E element);
+
+		
+	public void addAll(Collection<E> element);
+
+		
+	
+	public E remove(E element);
+
+	 
+	public void removeAll(Collection<E> element);
+
+	 
+	public void update(E element);
+
+	public void updateAll(Collection<E> element);
+	
+}
