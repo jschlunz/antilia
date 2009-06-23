916596c4729001b60e1e3bd60c9b12ade8c00bdf
reiern70 <reiern70@0c16656c-7d50-0410-8e61-d121633fcfaf>
2009-06-23 14:41:06 +0000

    clean Query related functionality off Hibernate references

---
+++ trunk/com.antilia.common/src/com/antilia/common/query/transform/IOrderTransformer.java
@@ -0,0 +1,17 @@
+/**
+ * This software is provided as IS by Antilia-Soft SL.
+ * Copyright 2006-2007.
+ */
+package com.antilia.common.query.transform;
+
+import java.io.Serializable;
+
+import com.antilia.common.query.IOrder;
+
+/**
+ * 
+ * @author Ernesto Reinaldo Barreiro (reiern70@gmail.com)
+ */
+public interface IOrderTransformer<R, B extends Serializable> extends ITransformer<IOrder<B>, R> {
+
+}
