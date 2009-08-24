b057edf15e2592dcc3d6abbf12e5600adc06b043
reiern70 <reiern70@0c16656c-7d50-0410-8e61-d121633fcfaf>
2009-08-24 06:28:04 +0000

    moved bubble to ext

---
+++ com.antilia.web.ext/src/com/antilia/web/bubble/Bubble.java
@@ -0,0 +1,36 @@
+/**
+ * This software is provided as IS by Antilia-Soft SL.
+ * Copyright 2006-2007.
+ */
+package com.antilia.web.bubble;
+
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
+
+/**
+ * 
+ *
+ * @author Ernesto Reinaldo Barreiro (reiern70@gmail.com)
+ *
+ */
+public class Bubble extends Panel {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @param id
+	 */
+	public Bubble(String id) {
+		super(id);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param id
+	 * @param model
+	 */
+	public Bubble(String id, IModel<?> model) {
+		super(id, model);
+	}
+
+}
