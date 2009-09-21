1e8111e6879d4ad17199677cb027e775c57e349f
reiern70 <reiern70@0c16656c-7d50-0410-8e61-d121633fcfaf>
2009-09-21 13:47:58 +0000

    http://www.dynamicdrive.com/style/csslibrary/item/jquery_multi_level_css_menu_2/

--- com.antilia.ext/src/com/antilia/web/dynamicdrive/test/TestMenu.java
+++ com.antilia.ext/src/com/antilia/web/dynamicdrive/test/TestMenu.java
@@ -4,6 +4,7 @@
  */
 package com.antilia.web.dynamicdrive.test;
 
+import org.apache.wicket.ResourceReference;
 import org.apache.wicket.markup.html.WebPage;
 
 import com.antilia.web.dynamicdrive.Menubar;
@@ -22,12 +23,22 @@ public class TestMenu extends WebPage  {
 	public TestMenu() {		
 		super();			
 		//add(new TestPanel("test"));		
-		Menubar menubar = new Menubar("test");
+		Menubar menubar = new Menubar("test") {
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			protected ResourceReference newMenuCss() {
+				return Menubar.SLIDEMENU_CSS_BLUE;
+			}
+		};
 		add(menubar);
 		
 		menubar.addItem(new ExternalLink("google", "http://www.google.com/ncr", "Google Inc"));
 		
-		SubMenubar subMenubar = new SubMenubar("One-1", menubar) {
+		SubMenubar subMenubar = new SubMenubar("Sub-Menu-One-1", menubar) {
 			/**
 			 * 
 			 */
@@ -35,12 +46,12 @@ public class TestMenu extends WebPage  {
 
 			@Override
 			protected String getTitle() {
-				return "One-1";
+				return "Sub-Menu-One-1";
 			}
 		};
 		
-		subMenubar.addItem(new ExternalLink("dynamicdrive", "http://www.dynamicdrive.com/style/csslibrary/item/jquery_multi_level_css_menu_2/","dynamicdrive1"));
-		subMenubar.addItem(new ExternalLink("dynamicdrive2", "http://www.dynamicdrive.com/style/csslibrary/item/jquery_multi_level_css_menu_2/","dynamicdrive2"));
+		subMenubar.addItem(new ExternalLink("dynamicdrive", "http://www.dynamicdrive.com/style/csslibrary/item/jquery_multi_level_css_menu_2/","Dynamicdrive1"));
+		subMenubar.addItem(new ExternalLink("dynamicdrive2", "http://www.dynamicdrive.com/style/csslibrary/item/jquery_multi_level_css_menu_2/","Dynamicdrive2"));
 		
 		subMenubar = new SubMenubar("Two-2", menubar) {
 			/**
@@ -50,11 +61,11 @@ public class TestMenu extends WebPage  {
 
 			@Override
 			protected String getTitle() {
-				return "Two-2";
+				return "Sub-Menu-Two-2";
 			}
 		};
-		subMenubar.addItem(new ExternalLink("dynamicdrive", "http://www.dynamicdrive.com/style/csslibrary/item/jquery_multi_level_css_menu_2/", "dynamicdrive"));
-		subMenubar.addItem(new ExternalLink("dynamicdrive2", "http://www.dynamicdrive.com/style/csslibrary/item/jquery_multi_level_css_menu_2/", "dynamicdrive2"));
+		subMenubar.addItem(new ExternalLink("dynamicdrive", "http://www.dynamicdrive.com/style/csslibrary/item/jquery_multi_level_css_menu_2/", "Dynamicdrive"));
+		subMenubar.addItem(new ExternalLink("dynamicdrive2", "http://www.dynamicdrive.com/style/csslibrary/item/jquery_multi_level_css_menu_2/", "Dynamicdrive2"));
 		
 
 		SubMenubar subMenubar2 = new SubMenubar("Two-2-1", subMenubar) {
@@ -67,8 +78,8 @@ public class TestMenu extends WebPage  {
 			}
 		};
 		
-		subMenubar2.addItem(new ExternalLink("dynamicdrive", "http://www.dynamicdrive.com/style/csslibrary/item/jquery_multi_level_css_menu_2/", "dynamicdrive"));
-		subMenubar2.addItem(new ExternalLink("dynamicdrive2", "http://www.dynamicdrive.com/style/csslibrary/item/jquery_multi_level_css_menu_2/", "dynamicdrive2"));
+		subMenubar2.addItem(new ExternalLink("dynamicdrive", "http://www.dynamicdrive.com/style/csslibrary/item/jquery_multi_level_css_menu_2/", "Dynamicdrive"));
+		subMenubar2.addItem(new ExternalLink("dynamicdrive2", "http://www.dynamicdrive.com/style/csslibrary/item/jquery_multi_level_css_menu_2/", "Dynamicdrive2"));
 
 		SubMenubar subMenubar3 = new SubMenubar("Two-2-1-1", subMenubar2) {
 			
@@ -80,10 +91,11 @@ public class TestMenu extends WebPage  {
 			}
 		};
 		
-		subMenubar3.addItem(new ExternalLink("dynamicdrive", "http://www.dynamicdrive.com/style/csslibrary/item/jquery_multi_level_css_menu_2/", "dynamicdrive"));
-		subMenubar3.addItem(new ExternalLink("dynamicdrive2", "http://www.dynamicdrive.com/style/csslibrary/item/jquery_multi_level_css_menu_2/", "dynamicdrive2"));
+		subMenubar3.addItem(new ExternalLink("dynamicdrive", "http://www.dynamicdrive.com/style/csslibrary/item/jquery_multi_level_css_menu_2/", "Dynamicdrive"));
+		subMenubar3.addItem(new ExternalLink("dynamicdrive2", "http://www.dynamicdrive.com/style/csslibrary/item/jquery_multi_level_css_menu_2/", "Dynamicdrive2"));
+		subMenubar3.addItem(new ExternalLink("google", "http://www.google.com/ncr", "Google Inc"));
 		
-		menubar.addItem(new ExternalLink("dynamicdrive2", "http://www.dynamicdrive.com/style/csslibrary/item/jquery_multi_level_css_menu_2/", "dynamicdrive2"));
+		menubar.addItem(new ExternalLink("dynamicdrive2", "http://www.dynamicdrive.com/style/csslibrary/item/jquery_multi_level_css_menu_2/", "Dynamicdrive2"));
 	}
 
 	
