0d4f6c864b1d8f1a3228704dbf14a0ca53e2c2bb
reiern70 <reiern70@0c16656c-7d50-0410-8e61-d121633fcfaf>
2009-10-28 16:19:27 +0000

    example of image read form somewhere else

---
+++ trunk/com.antilia.demo.manager/src/com/antilia/demo/manager/img/MyImageFactory.java
@@ -0,0 +1,75 @@
+package com.antilia.demo.manager.img;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.apache.wicket.ResourceReference;
+import org.apache.wicket.markup.html.image.Image;
+import org.apache.wicket.markup.html.image.resource.DynamicImageResource;
+
+/**
+ * 
+ * @author  Ernesto Reinaldo Barreiro (reiern70@gmail.com)
+ *
+ */
+public class MyImageFactory {
+
+	public static enum MODE {
+		FROM_CODE,
+		FROM_FOLDER
+	}
+	
+	static int BUFFER_SIZE = 10*1024;
+	
+	 /**
+     * Copies one stream into the other..
+	 * @param is	Stream fuente
+	 * @param os	Stream destino*/
+	static public void copy(InputStream is, OutputStream os) throws IOException {
+		byte[] buf = new byte[BUFFER_SIZE];
+		while (true) {
+			int tam = is.read(buf);
+			if (tam == -1) {
+				return;
+			}
+			os.write(buf, 0, tam);
+		}
+	}
+	
+	public static  byte[] bytes(InputStream is) throws IOException {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		copy(is, out);
+		return out.toByteArray();
+	}
+	
+	private static class ImageFromFolderWebResource extends DynamicImageResource {
+		
+		private static final long serialVersionUID = 1L;
+
+		private String name;
+		
+		public ImageFromFolderWebResource(String name) {
+			this.name = name;
+		}
+		
+		@Override
+		protected byte[] getImageData() {
+		try {
+				// instead of reading it form class path read it form a folder
+				return bytes(TestPage.class.getResourceAsStream(name));
+			} catch (Exception e) {
+				return null;
+			}
+		}
+	}
+	
+	public static Image createImage(String id, String imageName, MODE mode) {
+		if(mode.equals(MODE.FROM_CODE)) {
+			return new Image(id, new ResourceReference(MyImageFactory.class, imageName));
+		} else {
+			return new Image(id, new ImageFromFolderWebResource(imageName));
+		}
+	}
+}
