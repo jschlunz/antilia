873e70eb0ee2329edb55f2541f74492153d943a1
reiern70 <reiern70@0c16656c-7d50-0410-8e61-d121633fcfaf>
2009-12-01 15:18:11 +0000

    Questions editing

---
+++ com.antilia.letsplay/src/com/antilia/letsplay/model/mock/CabezaImage.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.antilia.letsplay.model.mock;
+
+import com.antilia.letsplay.model.ResourceImage;
+
+/**
+ * @author Ernesto Reinaldo Barreiro (reiern70@gmail.com)
+ *
+ */
+public class CabezaImage extends ResourceImage {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @param name
+	 */
+	public CabezaImage() {
+		super("head.jpg");
+	}
+
+}
