63f919689d7aebb72fe11fa69f2f38063aed51b9
reiern70 <reiern70@0c16656c-7d50-0410-8e61-d121633fcfaf>
2009-12-06 09:34:18 +0000

    fixing table

--- com.antilia.letsplay/src/com/antilia/letsplay/PlayPage.java
+++ com.antilia.letsplay/src/com/antilia/letsplay/PlayPage.java
@@ -3,6 +3,8 @@
  */
 package com.antilia.letsplay;
 
+import org.apache.wicket.markup.html.panel.Panel;
+
 import com.antilia.letsplay.components.ScrambledWordPanel;
 import com.antilia.web.login.IProtectedPage;
 
@@ -10,11 +12,14 @@ import com.antilia.web.login.IProtectedPage;
  * @author Ernesto Reinaldo Barreiro (reiern70@gmail.com)
  *
  */
-public class PlayPage extends BasePage implements IProtectedPage  {
+public class PlayPage extends RoundBasePage implements IProtectedPage  {
 
 	public PlayPage() {
 		super();
-		ScrambledWordPanel scrambled  = new ScrambledWordPanel("scrambled");
-		add(scrambled);
+	}
+
+	@Override
+	protected Panel newContentPanel(String id) {
+		return new ScrambledWordPanel(id);
 	}
 }
