b802a84eb2ea78aff7cc0a34d98ba1aff62d32e1
reiern70 <reiern70@0c16656c-7d50-0410-8e61-d121633fcfaf>
2010-01-27 20:41:38 +0000

    reading images form a folder

--- com.antilia.demo.manager/src/com/antilia/demo/manager/img/TestPage.java
+++ com.antilia.demo.manager/src/com/antilia/demo/manager/img/TestPage.java
@@ -5,8 +5,7 @@ package com.antilia.demo.manager.img;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.image.Image;
-
-import com.antilia.demo.manager.img.MyImageFactory.MODE;
+import org.apache.wicket.util.file.Folder;
 
 /**
  * @author  Ernesto Reinaldo Barreiro (reiern70@gmail.com)
@@ -14,12 +13,25 @@ import com.antilia.demo.manager.img.MyImageFactory.MODE;
  */
 public class TestPage extends WebPage {
 
+	private static final MountedImageFactory IMAGE_FACTORY = new MountedImageFactory() {
+		
+		@Override
+		protected Folder getFolder() {
+			return new Folder("C:/temp/images");
+		}
+		
+		@Override
+		protected String getMountPoint() {
+			return "test";
+		}
+		
+	};
 	
 	/**
 	 * 
 	 */
 	public TestPage() {
-		Image img = MyImageFactory.createImage("img", "test.png", MODE.FROM_FOLDER);
+		Image img = IMAGE_FACTORY.createImage("img", "test.png");
 		add(img);
 	}
 		
